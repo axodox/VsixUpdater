d9fd4105d54e34ecd099dfd129cd406a7c05e610
axodox <axodox@outlook.com>
2017-03-04 19:02:38 +0100

    Improved implementation

---
+++ VsixUpdater/Helpers.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.IO;
+using System.IO.Packaging;
+using System.Linq;
+using System.Security.Cryptography;
+
+namespace VsixUpdater
+{
+  public static class Helpers
+  {
+    public static string ReadAllText(this Package package, string path)
+    {
+      var part = package.GetPart(GetUri(path));
+      using (var partStream = part.GetStream(FileMode.Open, FileAccess.Read))
+      using (var reader = new StreamReader(partStream))
+      {
+        return reader.ReadToEnd();
+      }
+    }
+
+    public static void WriteAllText(this Package package, string path, string text)
+    {
+      var uri = GetUri(path);
+      var part = package.PartExists(uri) ?
+        package.GetPart(uri) :
+        package.CreatePart(uri, GetContentType(path), CompressionOption.Maximum);
+
+      using (var partStream = part.GetStream(FileMode.Create, FileAccess.Write))
+      using (var writer = new StreamWriter(partStream))
+      {
+        writer.Write(text);
+      }
+    }
+
+    public static byte[] ReadAllBytes(this Package package, string path)
+    {
+      var part = package.GetPart(GetUri(path));
+      using (var partStream = part.GetStream(FileMode.Open, FileAccess.Read))
+      {
+        var length = (int)partStream.Length;
+        var data = new byte[length];
+        partStream.Read(data, 0, length);
+        return data;
+      }
+    }
+
+    public static void WriteAllBytes(this Package package, string path, byte[] data)
+    {
+      var uri = GetUri(path);
+      var part = package.PartExists(uri) ?
+        package.GetPart(uri) :
+        package.CreatePart(uri, GetContentType(path), CompressionOption.Maximum);
+
+      using (var partStream = part.GetStream(FileMode.Create, FileAccess.Write))
+      {
+        partStream.Write(data, 0, data.Length);
+      }
+    }
+
+    public static string[] GetAllFiles(this Package package)
+    {
+      return package
+        .GetParts()
+        .Select(p => p.Uri.OriginalString)
+        .ToArray();
+    }
+
+    private static SHA256 _sha256 = SHA256Managed.Create();
+
+    public static string CalculateHash(this Package package, string path)
+    {
+      var part = package.GetPart(GetUri(path));
+      using (var partStream = part.GetStream(FileMode.Open, FileAccess.Read))
+      {
+        return string.Join(string.Empty, _sha256.ComputeHash(partStream).Select(p => p.ToString("X2")));
+      }
+    }
+
+    public static long GetSize(this Package package, string path)
+    {
+      var part = package.GetPart(GetUri(path));
+      using (var partStream = part.GetStream(FileMode.Open, FileAccess.Read))
+      {
+        return partStream.Length;
+      }
+    }
+
+    public static void Recompress(this Package package)
+    {
+      var filesToRecompress = package
+        .GetParts()
+        .Where(p => p.CompressionOption == CompressionOption.Maximum)
+        .Select(p => p.Uri)
+        .ToArray();
+
+      foreach (var file in filesToRecompress)
+      {
+        var data = package.ReadAllBytes(file.OriginalString);
+        package.DeletePart(file);
+        package.WriteAllBytes(file.OriginalString, data);
+      }
+    }
+
+    private static string GetContentType(string path)
+    {
+      var extension = Path
+        .GetExtension(path)
+        .TrimStart('.')
+        .ToLower();
+      switch (extension)
+      {
+        case "txt":
+        case "pkgdef":
+          return "text/plain";
+        case "xml":
+        case "vsixmanifest":
+          return "text/xml";
+        case "htm":
+        case "html":
+          return "text/html";
+        case "rtf":
+          return "application/rtf";
+        case "pdf":
+          return "application/pdf";
+        case "gif":
+          return "image/gif";
+        case "jpg":
+        case "jpeg":
+          return "image/jpg";
+        case "tiff":
+          return "image/tiff";
+        case "vsix":
+        case "zip":
+          return "application/zip";
+        default:
+          return "application/octet-stream";
+      }
+    }
+
+    private static Uri GetUri(string path)
+    {
+      return new Uri("/" + path.TrimStart('/'), UriKind.Relative);
+    }
+  }
+}
--- VsixUpdater/Program.cs
+++ VsixUpdater/Program.cs
@@ -1,10 +1,8 @@
 ﻿using Newtonsoft.Json;
 using System.Collections.Generic;
 using System.IO;
-using System.IO.Compression;
+using System.IO.Packaging;
 using System.Linq;
-using System.Security.Cryptography;
-using System.Text;
 using System.Xml.Linq;
 
 namespace VsixUpdater
@@ -13,20 +11,25 @@ namespace VsixUpdater
   {
     private const string _vsixTempDir = "vsixTemp";
     private const string _manifestName = "extension.vsixmanifest";
+    private const string _baseDependencyName = "Microsoft.VisualStudio.Component.CoreEditor";
+    private const string _baseDependencyVersion = "[15.0," + _nextVisualStudioVersion + ")";
+    private const string _baseDependencyDescription = "Visual Studio core editor";
+    private const string _nextVisualStudioVersion = "16.0";
+    private const string _catalogFilePath = "/catalog.json";
+    private const string _manifestFilePath = "/manifest.json";
+    private const string _packageType = "Vsix";
 
     static void Main(string[] args)
     {
       var vsixPath = args[0];
-      var rootPath = Path.GetDirectoryName(vsixPath);
-      var tempPath = Path.Combine(rootPath, _vsixTempDir);
       var installDirName = Path.GetRandomFileName();
 
-      //Extract zip file
-      ZipFile.ExtractToDirectory(vsixPath, tempPath);
+      //Open package
+      var package = Package.Open(vsixPath, FileMode.Open, FileAccess.ReadWrite);
 
       //Modify manifest
-      var manifestPath = Path.Combine(tempPath, _manifestName);
-      var manifestDocument = XDocument.Load(manifestPath);
+      var manifestText = package.ReadAllText(_manifestName);
+      var manifestDocument = XDocument.Parse(manifestText);
       var manifestNamespacePrefix = "{http://schemas.microsoft.com/developer/vsx-schema/2011}";
       var manifestElement = manifestDocument.Element(manifestNamespacePrefix + "PackageManifest");
       var installationElement = manifestElement.Element(manifestNamespacePrefix + "Installation");
@@ -42,148 +45,136 @@ namespace VsixUpdater
       {
         if (targetIds.Contains(target.Attribute("Id").Value))
         {
-          var versionParts = target.Attribute("Version").Value.Split(',');
-          var newVersion = string.Join(",", versionParts[0], "16.0)");
+          var oldVersion = target.Attribute("Version").Value;
+          var versionParts = oldVersion.Trim('(', ')', '[', ']').Split(',');
+          var newVersion = oldVersion[0] + versionParts[0] + _nextVisualStudioVersion + ')';
           target.Attribute("Version").Value = newVersion;
         }
       }
 
+      var dependencies = new Dictionary<string, string>();
+
       if (manifestElement.Element(manifestNamespacePrefix + "Prerequisites") == null)
       {
+        dependencies.Add(_baseDependencyName, _baseDependencyVersion);
+
         var prerequisitesElement = new XElement(manifestNamespacePrefix + "Prerequisites");
         var prerequisiteElement = new XElement(manifestNamespacePrefix + "Prerequisite");
-        prerequisiteElement.Add(new XAttribute("Id", "Microsoft.VisualStudio.Component.CoreEditor"));
-        prerequisiteElement.Add(new XAttribute("Version", "[15.0,16.0)"));
-        prerequisiteElement.Add(new XAttribute("DisplayName", "Visual Studio core editor"));
+        prerequisiteElement.Add(new XAttribute("Id", _baseDependencyName));
+        prerequisiteElement.Add(new XAttribute("Version", _baseDependencyVersion));
+        prerequisiteElement.Add(new XAttribute("DisplayName", _baseDependencyDescription));
         prerequisitesElement.Add(prerequisiteElement);
         manifestElement.Add(prerequisitesElement);
       }
+      else
+      {
+        var prerequisitesElement = manifestElement.Element(manifestNamespacePrefix + "Prerequisites");
+        var prerequisiteElements = prerequisitesElement.Descendants(manifestNamespacePrefix + "Prerequisite");
+        foreach (var prerequisiteElement in prerequisiteElements)
+        {
+          dependencies.Add(prerequisiteElement.Attribute("Id").Value, prerequisiteElement.Attribute("Version").Value);
+        }
+      }
 
-      manifestDocument.Save(manifestPath);
+      package.WriteAllText(_manifestName, manifestDocument.ToString());
 
       //Create catalog
-      var manifestInfo = ManifestInfo.FromManifest(manifestPath);
+      var manifestNode = manifestDocument.Element(manifestNamespacePrefix + "PackageManifest");
+      var metadataNode = manifestNode.Element(manifestNamespacePrefix + "Metadata");
+      var identityNode = metadataNode.Element(manifestNamespacePrefix + "Identity");
+
+      var packageId = identityNode.Attribute("Id").Value;
+      var packageTitle = metadataNode.Element(manifestNamespacePrefix + "DisplayName").Value;
+      var packageDescription = metadataNode.Element(manifestNamespacePrefix + "Description").Value;
+      var packageVersion = identityNode.Attribute("Version").Value;
 
       var exceptions = new[]
       {
-        "\\[Content_Types].xml",
-        "\\catalog.json",
-        "\\manifest.json"
+        _catalogFilePath,
+        _manifestFilePath
       };
 
-      var files = Directory.GetFiles(tempPath, "*.*", SearchOption.AllDirectories)
-        .Select(p => p.Substring(tempPath.Length))
+      var files = package
+        .GetAllFiles()
         .Except(exceptions)
-        .Where(p => !p.StartsWith("\\_rels\\") && !p.StartsWith("\\package\\"))
         .Select(p => new
         {
-          fileName = p.Replace('\\', '/'),
-          sha256 = CalculateHash(Path.Combine(tempPath, p.TrimStart('\\')))
+          path = p,
+          sha256 = package.CalculateHash(p)
         })
         .ToArray();
-      var installSize = files
-        .Select(p => Path.Combine(tempPath, p.fileName.Replace('/', '\\').TrimStart('\\')))
-        .Sum(p => new FileInfo(p).Length);
 
+      var installSize = files.Sum(p => package.GetSize(p.path));
+      var installationPath = $"[installdir]\\Common7\\IDE\\Extensions\\{installDirName}";
       var catalog = new
       {
         manifestVersion = "1.1",
-        info = new { id = $"{manifestInfo.Id},version={manifestInfo.Version}" },
+        info = new { id = $"{packageId},version={packageVersion}" },
         packages = new object[]
         {
           new {
-            id = "Component."+ manifestInfo.Id,
-            version = manifestInfo.Version,
+            id = "Component."+ packageId,
+            version = packageVersion,
             type = "Component",
             extension = true,
-            dependencies = new Dictionary<string, string>()
+            dependencies = new global::System.Collections.Generic.Dictionary<string, string>(dependencies)
             {
-              {manifestInfo.Id, manifestInfo.Version },
-              {"Microsoft.VisualStudio.Component.CoreEditor", "[15.0,16.0)" }
+              { packageId, packageVersion }
             },
             localizedResources = new []
             {
               new
               {
                 language = "en-US",
-                title = manifestInfo.Title,
-                description = manifestInfo.Description
+                title = packageTitle,
+                description = packageDescription
               }
             }
           },
           new
           {
-            id = manifestInfo.Id,
-            version = manifestInfo.Version,
-            type = "Vsix",
+            id = packageId,
+            version = packageVersion,
+            type = VsixUpdater.Program._packageType,
             payloads = new []
             {
               new
               {
-                fileName = Path.GetFileName(vsixPath),
+                fileName = global::System.IO.Path.GetFileName(vsixPath),
                 size = installSize
               }
             },
-            vsixId = manifestInfo.Id,
-            extensionDir = $"[installdir]\\Common7\\IDE\\Extensions\\{installDirName}",
+            vsixId = packageId,
+            extensionDir = installationPath,
             installSize = installSize,
           }
         }
 
       };
       var jsonCatalog = JsonConvert.SerializeObject(catalog, Formatting.Indented);
-      File.WriteAllText(Path.Combine(tempPath, "catalog.json"), jsonCatalog);
+      package.WriteAllText(_catalogFilePath, jsonCatalog);
 
       //Create manifest
       var manifest = new
       {
-        id = manifestInfo.Id,
-        version = manifestInfo.Version,
-        type = "Vsix",
-        vsixId = manifestInfo.Id,
-        extensionDir = $"[installdir]\\Common7\\IDE\\Extensions\\{installDirName}",
+        id = packageId,
+        version = packageVersion,
+        type = _packageType,
+        vsixId = packageId,
+        extensionDir = installationPath,
         files = files,
         installSize = installSize,
-        dependencies = new Dictionary<string, string>()
-        {
-          {"Microsoft.VisualStudio.Component.CoreEditor", "[15.0,16.0)" }
-        }
+        dependencies = dependencies
       };
       var jsonManifest = JsonConvert.SerializeObject(manifest, Formatting.Indented);
-      File.WriteAllText(Path.Combine(tempPath, "manifest.json"), jsonManifest);
+      package.WriteAllText(_manifestFilePath, jsonManifest);
 
-      //Update content types
-      var typesPath = Path.Combine(tempPath, "[Content_Types].xml");
-      var typesDocument = XDocument.Load(typesPath);
-      var typesNamespacePrefix = "{http://schemas.openxmlformats.org/package/2006/content-types}";
-      var typesElement = typesDocument.Element(typesNamespacePrefix + "Types");
-      var jsonElement = typesElement
-        .Descendants(typesNamespacePrefix + "Default")
-        .FirstOrDefault(p => p.Attribute("Extension").Value == "json");
-      if (jsonElement == null)
-      {
-        jsonElement = new XElement(typesNamespacePrefix + "Default");
-        jsonElement.Add(new XAttribute("Extension", "json"));
-        jsonElement.Add(new XAttribute("ContentType", "application/json"));
-        typesElement.Add(jsonElement);
-      }
-      typesDocument.Save(typesPath);
-      //Create zip
-      File.Delete(vsixPath);
-      ZipFile.CreateFromDirectory(tempPath, vsixPath, CompressionLevel.Optimal, false, Encoding.ASCII);
+      //Recompress
+      package.Recompress();
 
-      //CleanUp
-      Directory.Delete(tempPath, true);
-    }
-
-    private static SHA256 _sha256 = SHA256Managed.Create();
-
-    private static string CalculateHash(string filePath)
-    {
-      using (var stream = new FileStream(filePath, FileMode.Open, FileAccess.Read))
-      {
-        return string.Join(string.Empty, _sha256.ComputeHash(stream).Select(p => p.ToString("X2")));
-      }
+      //Save
+      package.Flush();
+      package.Close();
     }
   }
 }
